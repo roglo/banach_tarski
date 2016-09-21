1180f6a867eb39b86396ebe5d379f9f0bbb2373b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-09-21 10:55:20 +0200

    -

--- banach_tarski.v
+++ banach_tarski.v
@@ -1825,6 +1825,7 @@ intros p p₁ p₂ el₁ el₂ Hexcl Hp₁ Hp₂ Hn Hp.
    proof is based upon the fact that its coordinates are integers, which
    is not necessarily the case for any p.
  *)
+bbb.
 Abort.
 
 Definition no_rotation := ([] : list free_elem).
