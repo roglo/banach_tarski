a9dceeced485f03b270e5258adafc5147e543d4a
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-09-26 16:40:13 +0200

    -

--- banach_tarski.v
+++ banach_tarski.v
@@ -2141,121 +2141,108 @@ Qed.
    - wikipedia "rotation matrix"
    - http://www.euclideanspace.com/maths/geometry/rotations/
        conversions/matrixToAngle *)
-Definition matrix_fixpoint (m : matrix) :=
+Definition rotation_fixpoint (m : matrix) k :=
   let x := (a₃₂ m - a₂₃ m)%R in
   let y := (a₁₃ m - a₃₁ m)%R in
   let z := (a₂₁ m - a₁₂ m)%R in
-  let r := √ (x² + y² + z²)%R in
-  P (x/r) (y/r) (z/r).
+  P (k * x) (k * y) (k * z).
 
-Theorem matrix_fixpoint_ok : ∀ m p,
+Theorem matrix_fixpoint_ok : ∀ m p k,
   is_rotation_matrix m
-  → p = matrix_fixpoint m
-  → p ≠ P 0 0 0
+  → p = rotation_fixpoint m k
   → mat_vec_mul m p = p.
 Proof.
-intros m p Hrm Hp Hn.
+intros m p k Hrm Hn.
 subst p.
-unfold matrix_fixpoint in Hn.
-unfold matrix_fixpoint.
+unfold rotation_fixpoint.
 remember (a₃₂ m - a₂₃ m)%R as x eqn:Hx.
 remember (a₁₃ m - a₃₁ m)%R as y eqn:Hy.
 remember (a₂₁ m - a₁₂ m)%R as z eqn:Hz.
-remember (√ (x² + y² + z²)%R) as r eqn:Hr.
-assert (Hrnz : (r ≠ 0)%R).
- intros H; apply Hn; clear Hn.
- move H at top; subst r.
- symmetry in Hr.
- apply sqrt_eq_0 in Hr.
-  assert (H : (x = 0)%R).
-   apply Rsqr_0_uniq.
-   rewrite Rplus_assoc in Hr.
-   apply Rplus_eq_0_l with (y²+z²)%R; [ apply Rle_0_sqr | | assumption ].
-   apply Rplus_le_le_0_compat; apply Rle_0_sqr.
-
-   move H at top; subst x.
-   rewrite Rsqr_0, Rplus_0_l in Hr.
-   apply Rplus_sqr_eq_0 in Hr.
-   move Hr at top; destruct Hr; subst y z.
-   f_equal; lra.
-
-  do 3 rewrite Rsqr_pow2.
-  rewrite Rplus_assoc.
-  apply Rplus_le_le_0_compat; [ apply pow2_ge_0 | ].
-  apply Rplus_le_le_0_compat; apply pow2_ge_0.
-
- unfold is_rotation_matrix in Hrm.
- destruct Hrm as (Ht & Hd).
- unfold mat_det in Hd.
- unfold mat_mul, mat_transp, mat_id in Ht; simpl in Ht.
- injection Ht; clear Ht; intros H₁ H₂ H₃ H₄ H₅ H₆ H₇ H₈ H₉.
- simpl.
- setoid_rewrite fold_Rsqr in H₁.
- setoid_rewrite fold_Rsqr in H₅.
- setoid_rewrite fold_Rsqr in H₉.
- move H₉ after H₁; move H₅ after H₁.
- move H₄ before H₂; move H₇ before H₃; move H₈ before H₆.
- setoid_rewrite Rmult_comm in H₂.
- setoid_rewrite Rmult_comm in H₇.
- setoid_rewrite Rmult_comm in H₆.
- clear H₄ H₇ H₈; move H₆ after H₂.
- move Hd before H₉.
- remember (a₁₁ m) as m₁₁.
- remember (a₁₂ m) as m₁₂.
- remember (a₁₃ m) as m₁₃.
- remember (a₂₁ m) as m₂₁.
- remember (a₂₂ m) as m₂₂.
- remember (a₂₃ m) as m₂₃.
- remember (a₃₁ m) as m₃₁.
- remember (a₃₂ m) as m₃₂.
- remember (a₃₃ m) as m₃₃.
- replace
-   (m₁₁ * (m₂₂ * m₃₃ - m₃₂ * m₂₃) + m₁₂ * (m₂₃ * m₃₁ - m₃₃ * m₂₁) +
-    m₁₃ * (m₂₁ * m₃₂ - m₃₁ * m₂₂))%R
- with
- (m₁₁ * m₂₂ * m₃₃ - m₁₁ * m₂₃ * m₃₂ + m₁₂ * m₂₃ * m₃₁ - m₁₂ * m₂₁ * m₃₃ +
-  m₁₃ * m₂₁ * m₃₂ - m₁₃ * m₂₂ * m₃₁)%R in Hd by ring.
- move r before m.
- move m₃₃ before m; move m₃₂ before m; move m₃₁ before m.
- move m₂₃ before m; move m₂₂ before m; move m₂₁ before m.
- move m₁₃ before m; move m₁₂ before m; move m₁₁ before m.
- move Heqm₁₁ after Heqm₂₃; move Heqm₂₂ after Heqm₂₃; move Heqm₃₃ after Heqm₂₃.
- rename H₆ into H₁₁; rename H₂ into H₂₁; rename H₃ into H₃₁.
- rename H₁ into H₃; rename H₅ into H₂; rename H₉ into H₁.
+unfold is_rotation_matrix in Hrm.
+destruct Hrm as (Ht & Hd).
+unfold mat_det in Hd.
+unfold mat_mul, mat_transp, mat_id in Ht; simpl in Ht.
+injection Ht; clear Ht; intros H₁ H₂ H₃ H₄ H₅ H₆ H₇ H₈ H₉.
+simpl.
+setoid_rewrite fold_Rsqr in H₁.
+setoid_rewrite fold_Rsqr in H₅.
+setoid_rewrite fold_Rsqr in H₉.
+move H₉ after H₁; move H₅ after H₁.
+move H₄ before H₂; move H₇ before H₃; move H₈ before H₆.
+setoid_rewrite Rmult_comm in H₂.
+setoid_rewrite Rmult_comm in H₇.
+setoid_rewrite Rmult_comm in H₆.
+clear H₄ H₇ H₈; move H₆ after H₂.
+move Hd before H₉.
+remember (a₁₁ m) as m₁₁.
+remember (a₁₂ m) as m₁₂.
+remember (a₁₃ m) as m₁₃.
+remember (a₂₁ m) as m₂₁.
+remember (a₂₂ m) as m₂₂.
+remember (a₂₃ m) as m₂₃.
+remember (a₃₁ m) as m₃₁.
+remember (a₃₂ m) as m₃₂.
+remember (a₃₃ m) as m₃₃.
+replace
+  (m₁₁ * (m₂₂ * m₃₃ - m₃₂ * m₂₃) + m₁₂ * (m₂₃ * m₃₁ - m₃₃ * m₂₁) +
+   m₁₃ * (m₂₁ * m₃₂ - m₃₁ * m₂₂))%R
+with
+(m₁₁ * m₂₂ * m₃₃ - m₁₁ * m₂₃ * m₃₂ + m₁₂ * m₂₃ * m₃₁ - m₁₂ * m₂₁ * m₃₃ +
+ m₁₃ * m₂₁ * m₃₂ - m₁₃ * m₂₂ * m₃₁)%R in Hd by ring.
+move m₃₃ before m; move m₃₂ before m; move m₃₁ before m.
+move m₂₃ before m; move m₂₂ before m; move m₂₁ before m.
+move m₁₃ before m; move m₁₂ before m; move m₁₁ before m.
+move Heqm₁₁ after Heqm₂₃; move Heqm₂₂ after Heqm₂₃; move Heqm₃₃ after Heqm₂₃.
+rename H₆ into H₁₁; rename H₂ into H₂₁; rename H₃ into H₃₁.
+rename H₁ into H₃; rename H₅ into H₂; rename H₉ into H₁.
+f_equal.
+ field_simplify.
  f_equal.
-  field_simplify; [ | assumption | assumption ].
-  f_equal.
-  subst x y z.
-  ring_simplify.
-  apply Rplus_eq_reg_r with (- (m₃₂ - m₂₃))%R.
-  ring_simplify.
-  rewrite <- Rmult_minus_distr_l.
-  replace (m₁₁ * (m₃₂ - m₂₃) - m₃₂ + m₂₃ - m₁₂ * m₃₁ + m₁₃ * m₂₁)%R
-  with ((m₁₁ - 1) * (m₃₂ - m₂₃) + m₁₃ * m₂₁ - m₁₂ * m₃₁)%R by ring.
-  clear - Hd H₁ H₂ H₃ H₁₁ H₂₁ H₃₁.
-  nsatz.
+ subst x y z.
+ ring_simplify.
+ apply Rplus_eq_reg_r with (- (k * (m₃₂ - m₂₃)))%R.
+ ring_simplify.
+ rewrite <- Rmult_minus_distr_l.
+ replace (m₁₁ * k)%R with (k * m₁₁)%R by apply Rmult_comm.
+ setoid_rewrite Rmult_assoc.
+ rewrite <- Rmult_minus_distr_l, <- Rmult_plus_distr_l.
+ rewrite <- Rmult_minus_distr_l, <- Rmult_plus_distr_l.
+ replace (m₁₁ * (m₃₂ - m₂₃) - m₃₂ + m₂₃ - m₁₂ * m₃₁ + m₁₃ * m₂₁)%R
+ with ((m₁₁ - 1) * (m₃₂ - m₂₃) + m₁₃ * m₂₁ - m₁₂ * m₃₁)%R by ring.
+ clear - Hd H₁ H₂ H₃ H₁₁ H₂₁ H₃₁.
+ nsatz.
 
-  field_simplify; [ | assumption | assumption ].
-  f_equal.
-  subst x y z.
-  ring_simplify.
-  apply Rplus_eq_reg_r with (- (m₁₃ - m₃₁))%R.
-  ring_simplify.
-  replace (m₂₁ * m₃₂ - m₂₃ * m₁₂ + m₂₂ * m₁₃ - m₂₂ * m₃₁ - m₁₃ + m₃₁)%R
-  with ((m₂₂ - 1) * (m₁₃ - m₃₁) + m₂₁ * m₃₂ - m₂₃ * m₁₂)%R by ring.
-  clear - Hd H₁ H₂ H₃ H₁₁ H₂₁ H₃₁.
-  nsatz.
+ field_simplify.
+ f_equal.
+ subst x y z.
+ ring_simplify.
+ apply Rplus_eq_reg_r with (- k * (m₁₃ - m₃₁))%R.
+ ring_simplify.
+ replace (m₂₁ * k)%R with (k * m₂₁)%R by apply Rmult_comm.
+ setoid_rewrite Rmult_assoc.
+ rewrite <- Rmult_minus_distr_l, <- Rmult_plus_distr_l.
+ rewrite <- Rmult_minus_distr_l, <- Rmult_minus_distr_l.
+ rewrite <- Rmult_plus_distr_l.
+ replace (m₂₁ * m₃₂ - m₂₃ * m₁₂ + m₂₂ * m₁₃ - m₂₂ * m₃₁ - m₁₃ + m₃₁)%R
+ with ((m₂₂ - 1) * (m₁₃ - m₃₁) + m₂₁ * m₃₂ - m₂₃ * m₁₂)%R by ring.
+ clear - Hd H₁ H₂ H₃ H₁₁ H₂₁ H₃₁.
+ nsatz.
 
-  field_simplify; [ | assumption | assumption ].
-  f_equal.
-  subst x y z.
-  ring_simplify.
-  apply Rplus_eq_reg_r with (- (m₂₁ - m₁₂))%R.
-  ring_simplify.
-  replace (- m₃₁ * m₂₃ + m₃₂ * m₁₃ + m₃₃ * m₂₁ - m₃₃ * m₁₂ - m₂₁ + m₁₂)%R
-  with ((m₃₃ - 1) * (m₂₁ - m₁₂) + m₃₂ * m₁₃ - m₂₃ * m₃₁)%R by ring.
-  clear - Hd H₁ H₂ H₃ H₁₁ H₂₁ H₃₁.
-  nsatz.
+ field_simplify.
+ f_equal.
+ subst x y z.
+ ring_simplify.
+ apply Rplus_eq_reg_r with (- k * (m₂₁ - m₁₂))%R.
+ ring_simplify.
+ replace (- m₃₁ * k)%R with (k * - m₃₁)%R by apply Rmult_comm.
+ setoid_rewrite Rmult_assoc.
+ rewrite <- Rmult_plus_distr_l, <- Rmult_plus_distr_l.
+ rewrite <- Rmult_minus_distr_l, <- Rmult_minus_distr_l.
+ rewrite <- Rmult_plus_distr_l.
+ replace (- m₃₁ * m₂₃ + m₃₂ * m₁₃ + m₃₃ * m₂₁ - m₃₃ * m₁₂ - m₂₁ + m₁₂)%R
+ with ((m₃₃ - 1) * (m₂₁ - m₁₂) + m₃₂ * m₁₃ - m₂₃ * m₃₁)%R by ring.
+ clear - Hd H₁ H₂ H₃ H₁₁ H₂₁ H₃₁.
+ nsatz.
 Qed.
 
 bbb.
