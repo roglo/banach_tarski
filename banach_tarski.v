7630a464cb53529294332139c818652e0e23ac65
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-10-20 15:19:29 +0200

    -

--- banach_tarski.v
+++ banach_tarski.v
@@ -7,53 +7,9 @@
 
 Require Import Utf8 List Relations NPeano Wf_nat.
 Import ListNotations.
-Require Import Misc Words Normalize Reverse.
-
-(* Misc about Reals *)
-
 Require Import Reals Psatz Nsatz.
 
-Notation "'ℝ'" := R.
-Notation "'ℤ'" := Z.
-Notation "'ℕ'" := nat.
-
-Notation "'√'" := sqrt.
-
-Theorem fold_Rsqr : ∀ a, (a * a = a²)%R.
-Proof. reflexivity. Qed.
-
-Theorem Rmul_div : ∀ x y z, (x * y / z = x / z * y)%R.
-Proof. intros; lra. Qed.
-
-Theorem Rmult_shuffle0 : ∀ n m p : ℝ, (n * m * p)%R = (n * p * m)%R.
-Proof.
-intros.
-rewrite Rmult_comm, <- Rmult_assoc.
-f_equal; apply Rmult_comm.
-Qed.
-
-Theorem Req_dec : ∀ x y : ℝ, { (x = y)%R } + { (x ≠ y)%R }.
-Proof.
-intros x y.
-destruct (Rle_dec x y) as [H₁| H₁].
- destruct (Rle_dec y x) as [H₂| H₂].
-  left; apply Rle_antisym; assumption.
-
-  right; intros H; subst y; apply H₂, Rle_refl.
-
- right; intros H; subst y.
- apply H₁, Rle_refl.
-Qed.
-
-Theorem Rmult5_sqrt2_sqrt5 : ∀ a b c d, (0 <= b)%R →
-  (a * √ b * c * d * √ b)%R = (a * b * c * d)%R.
-Proof.
-intros a b c d Hb.
-rewrite Rmult_comm, <- Rmult_assoc; f_equal.
-rewrite <- Rmult_assoc; f_equal.
-rewrite Rmult_comm, Rmult_assoc; f_equal.
-apply sqrt_sqrt; assumption.
-Qed.
+Require Import Misc Words Normalize Reverse MiscReals.
 
 (* Matrices and Points *)
 
