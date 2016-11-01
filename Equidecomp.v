0d7a25190beaeb2c2ced75c4be25918bfe70e2bc
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-11-01 14:24:27 +0100

    -

--- Equidecomp.v
+++ Equidecomp.v
@@ -553,7 +553,7 @@ split.
         remember intersection as f; simpl in Hy; subst f.
         clear IHHpe1 Hik.
         revert E₁ E₂ y k Hpai Hy.
-        induction Hpe1 as [| E₃ PE P'E | E₃ E₄ PE | PE P'E P''E ]; intros.
+        induction Hpe1 as [| E₃ PE P'E | E₃ E₄ PE | PE P'E P''E]; intros.
          simpl in Hy; rewrite match_id in Hy; easy.
 
          destruct k.
@@ -578,7 +578,20 @@ split.
             apply Nat.succ_inj_wd_neg in Hij.
             now apply (Hpai (S (S (S i))) (S (S (S j)))).
 
-         simpl.
+         destruct k; [ now apply (Hpai 1 3 (n_SSn 1) y) | ].
+         destruct k; [ now apply (Hpai 1 2 (n_Sn 1) y) | ].
+         assert (Hk : 1 ≠ S (S (S (S k)))) by easy.
+         now apply (Hpai 1 (S (S (S (S k)))) Hk y).
+
+         eapply IHHpe1_2 with (E₁ := E₁); [ | eassumption ].
+         clear y k Hy.
+         intros i j Hij.
+         destruct i.
+          destruct j; [ exfalso; now apply Hij | ].
+          destruct j; [ now apply (Hpai 0 1 Hij) | simpl ].
+          intros x; split; [ intros Hx; simpl | easy ].
+          eapply IHHpe1_1 with (E₁ := E₂); [ | eassumption ].
+          intros i k Hik.
 bbb.
 
       destruct k; [ now apply (Hpai 0 1 Hij x) | ].
