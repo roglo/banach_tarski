2fc4911e7518b8de14ee012f76ab40e790642774
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-11-05 05:26:29 +0100

    -

--- Equidecomp.v
+++ Equidecomp.v
@@ -48,22 +48,6 @@ Definition partition_combine {A B} (fl : list (set A → set B)) PE PF :=
 Definition partition_combine_swi {A B} (fl : list (set A → set B)) PE PF :=
   flat_map (λ Fj, map (λ '(fi, Ei), Ei ∩ fi Fj) (combine fl PE)) PF.
 
-Definition partition_prod {A} (PE PF : list (set A)) :=
-  flat_map (λ p, map (intersection p) PF) PE.
-
-Definition partition_prod2 {A} (PE PF : list (set A)) :=
-  partition_combine (map (λ _ E, E) PE) PE PF.
-
-Theorem equiv_partition_prod_prod2 : ∀ A (PE PF : list (set A)),
-  partition_prod PE PF = partition_prod2 PE PF.
-Proof.
-intros.
-unfold partition_prod, partition_prod2.
-unfold partition_combine.
-induction PE as [| E₁ PE]; intros; [ destruct PF; reflexivity | simpl ].
-f_equal; apply IHPE.
-Qed.
-
 Theorem partition_combine_nth :
   ∀ A (s := set_equiv) fl (PE : list (set A)) PF i len,
   len = length PF
@@ -136,58 +120,6 @@ induction PE as [| E₁ PE]; intros.
     intros f Hfi.
     apply Hf; right; assumption.
 Qed.
-
-Theorem partition_combine_swi_nth_from_combine :
-  ∀ A (s := set_equiv) fl (PE PF : list (set A)) i lenE lenF,
-  lenE = length PE
-  → lenF = length PF
-  → length fl = length PE
-  → (∀ f, List.In f fl → (f ∅ = ∅)%S)
-  → ((partition_combine_swi fl PE PF).[i] =
-     (partition_combine fl PE PF).[(i mod lenE) * lenF + i / lenE])%S.
-Proof.
-intros * HlenE HlenF Hflp Hf.
-rewrite partition_combine_nth; try eassumption.
-destruct (eq_nat_dec lenE 0) as [HEz| HEnz].
- subst lenE.
- apply length_zero_iff_nil in HEz; subst PE.
- apply length_zero_iff_nil in Hflp; subst fl.
- simpl; do 2 rewrite match_id.
- unfold partition_combine_swi; simpl.
- rewrite flat_map_nil_fun; simpl.
-  rewrite match_id; now rewrite intersection_empty_l.
-
-  clear; induction PF; now constructor; easy.
-
- destruct (eq_nat_dec lenF 0) as [HFz| HFnz].
-  subst lenF.
-  apply length_zero_iff_nil in HFz; subst PF.
-  simpl; rewrite match_id.
-  rewrite Nat.mul_0_r, Nat.add_0_l.
-  assert (Hj : ∀ j, (nth j fl id ∅ = ∅)%S).
-   clear -Hf.
-   induction fl as [| f₁ fl]; intros; [ simpl; now rewrite match_id | simpl ].
-   destruct j.
-    rewrite Hf; [ easy | now left ].
-
-    apply IHfl.
-    intros f Hfi.
-    apply Hf; now right.
-
-   now rewrite Hj, intersection_empty_r.
-
-  rewrite Nat.div_add_l; [ | easy ].
-  setoid_rewrite Nat.add_comm.
-  rewrite Nat.mod_add; [ | easy ].
-  rewrite Nat.div_div; [ | easy | easy ].
-  destruct (lt_dec i (lenE * lenF)) as [Hi| Hi].
-   rewrite Nat.div_small; [ | easy ].
-   rewrite Nat.add_0_l.
-   replace ((i / lenE) mod lenF) with (i / lenE).
- Focus 2.
- rewrite Nat.mod_small; [ easy | ].
- now apply Nat.div_lt_upper_bound.
-Abort.
 
 Theorem partition_combine_swi_nth :
   ∀ A (s := set_equiv) fl (PE : list (set A)) PF i len,
@@ -493,23 +425,6 @@ apply partition_combine_partition_combine_swi.
 eapply partition_combine_is_partition with (F := F) (PF := PF); eassumption.
 Qed.
 
-Theorem partition_prod_nil_l : ∀ A (Q : list (set A)),
-  partition_prod [] Q = [].
-Proof. reflexivity. Qed.
-
-Theorem partition_prod_nil_r : ∀ A (P : list (set A)),
-  partition_prod P [] = [].
-Proof.
-intros A P.
-unfold partition_prod.
-induction P as [| P PL]; [ reflexivity | apply IHPL ].
-Qed.
-
-Theorem partition_prod_cons_l : ∀ A P (PL QL : list (set A)),
-  partition_prod (P :: PL) QL =
-  map (intersection P) QL ++ partition_prod PL QL.
-Proof. reflexivity. Qed.
-
 Theorem partition_combine_length :
   ∀ A fl (PE PF : list (set A)),
   length fl = length PE
@@ -541,137 +456,6 @@ intros * Hlen.
 pose proof partition_combine_swi_is_permutation _ fl PE PF as H.
 apply Permutation_length in H.
 now rewrite H; apply partition_combine_length.
-Qed.
-
-Theorem partition_prod_length :
-  ∀ A (P Q : list (set A)),
-  length (partition_prod P Q) = (length P * length Q)%nat.
-Proof.
-intros A P Q.
-revert Q.
-induction P as [| P PL]; intros; [ reflexivity | simpl ].
-rewrite app_length, IHPL, map_length.
-reflexivity.
-Qed.
-
-Theorem partition_prod_nth :
-  ∀ A (s := set_equiv) (PL QL : list (set A)) len i,
-  len = length QL
-  → ((partition_prod PL QL).[i] = PL.[i / len] ∩ QL.[i mod len])%S.
-Proof.
-(* see if I could reuse partition_combine_nth; but, perhaps the present
-   theorem is going to be removed later, if no more used *)
-intros * Hlen.
-subst len.
-revert QL i.
-induction PL as [| P PL]; intros.
- intros x.
- split; intros Hx; [ destruct i; contradiction | ].
- destruct Hx as (Hx, _).
- destruct (i / length QL)%nat; contradiction.
-
- rewrite partition_prod_cons_l.
- destruct (lt_dec i (length QL)) as [Hi| Hi].
-  rewrite app_nth1; [| rewrite map_length; assumption ].
-  rewrite Nat.div_small; [ simpl | assumption ].
-  rewrite Nat.mod_small; [ simpl | assumption ].
-  intros x; clear.
-  split; intros Hx.
-   revert i Hx.
-   induction QL as [| Q QL]; intros; [ destruct i; contradiction | ].
-   simpl in Hx; simpl.
-   destruct i; [ assumption | apply IHQL; assumption ].
-
-   revert i Hx.
-   induction QL as [| Q QL]; intros.
-    destruct Hx; destruct i; contradiction.
-
-    destruct i; simpl in Hx; simpl; [ assumption | ].
-    apply IHQL; assumption.
-
-  apply Nat.nlt_ge in Hi.
-  rewrite app_nth2; [| rewrite map_length; assumption ].
-  rewrite map_length.
-  remember (i - length QL)%nat as j eqn:Hj.
-  assert (H : (i = j + length QL)%nat).
-   rewrite Hj.
-   rewrite Nat.sub_add; [ reflexivity | assumption ].
-
-   subst i; clear Hi Hj.
-   destruct QL as [| Q QL].
-    rewrite partition_prod_nil_r; simpl.
-    intros x.
-    split; intros Hx; [ destruct j; contradiction | ].
-    destruct Hx; destruct j; contradiction.
-
-    rewrite nat_mod_add_once; [ | intros H; discriminate H ].
-    rewrite nat_div_add_once; [ | intros H; discriminate H ].
-    apply IHPL.
-Qed.
-
-Theorem partition_prod_is_partition : ∀ A (s := set_equiv) (E : set A) P Q,
-  is_partition E P → is_partition E Q → is_partition E (partition_prod P Q).
-Proof.
-intros A s E P Q (HEP, HPij) (HEQ, HQij).
-split.
- intros x.
- split; intros H.
-  generalize H; intros HP; apply HEP in HP.
-  generalize H; intros HQ; apply HEQ in HQ.
-  clear - s HP HQ.
-  induction P as [| P PL]; [ contradiction | simpl in HP ].
-  destruct HP as [HP| HP].
-   rewrite partition_prod_cons_l, union_list_app; [ | reflexivity ].
-   left; eapply union_list_intersection; assumption.
-
-   rewrite partition_prod_cons_l, union_list_app; [ | reflexivity ].
-   right; apply IHPL; assumption.
-
-  clear - HEP HEQ H.
-  revert E Q HEP HEQ H.
-  induction P as [| P PL]; intros; [ contradiction | ].
-  rewrite partition_prod_cons_l, union_list_app in H; [ | reflexivity ].
-  apply HEP; simpl.
-  destruct H as [H| H].
-   left.
-   clear -H.
-   induction Q as [| Q QL]; [ contradiction | ].
-   destruct H as [H| H]; [ destruct H; assumption | apply IHQL, H ].
-
-   right.
-   clear -s H.
-   revert Q H.
-   induction PL as [| P PL]; intros; [ contradiction | ].
-   rewrite partition_prod_cons_l, union_list_app in H; [ | reflexivity ].
-   destruct H as [H| H].
-    left.
-    clear -H.
-    induction Q as [| Q QL]; [ contradiction | ].
-    destruct H as [H| H]; [ destruct H; assumption | apply IHQL, H ].
-
-    right.
-    eapply IHPL, H.
-
- intros i j Hij.
- split; [ | intros H; contradiction ].
- erewrite partition_prod_nth; [ | reflexivity ].
- erewrite partition_prod_nth; [ | reflexivity ].
- remember (length Q) as len eqn:Hlen.
- destruct Q as [| Q QL]; [ intros (_ & _ & H); subst len; contradiction | ].
- simpl in Hlen.
- intros Hx; simpl.
- destruct Hx as ((Hpi, Hqi), (Hpj, Hqj)).
- destruct (eq_nat_dec (i / len) (j / len)) as [Hd| Hd].
-  destruct (eq_nat_dec (i mod len) (j mod len)) as [Hm| Hm].
-   assert (Hnlen : (len ≠ 0)%nat) by (subst len; intros H; discriminate H).
-   pose proof Nat.div_mod i len Hnlen as Hi.
-   pose proof Nat.div_mod j len Hnlen as Hj.
-   rewrite Hd, Hm, <- Hj in Hi.
-   contradiction.
-
-   eapply HQij; [ apply Hm | split; eassumption ].
-
-  eapply HPij; [ apply Hd | split; eassumption ].
 Qed.
 
 Require Import Setoid.
