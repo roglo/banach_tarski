1786b4a078671be3363a039df82ca93ababd9446
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-11-07 08:20:07 +0100

    -

--- Banach_Tarski.v
+++ Banach_Tarski.v
@@ -150,64 +150,50 @@ Qed.
 Definition rotate_set axis ang E :=
   mkset (λ p, mat_vec_mul (rot_mat_of_axis_cos axis (-cos ang)) p ∈ E).
 
-Fixpoint path_of_pos p :=
-  match p with
-  | xO (xO p) => ạ :: path_of_pos p
-  | xI (xO p) => ạ⁻¹ :: path_of_pos p
-  | xO (xI p) => ḅ :: path_of_pos p
-  | xI (xI p) => ḅ⁻¹ :: path_of_pos p
-  | _ => []
+Fixpoint path_of_nat_aux it n :=
+  match it with
+  | O => []
+  | S it' =>
+      let l :=
+        match n / 4 with
+        | 0 => []
+        | S n' => path_of_nat_aux it' n'
+        end
+      in
+      match n mod 4 with
+      | 0 => ạ :: l
+      | 1 => ạ⁻¹ :: l
+      | 2 => ḅ :: l
+      | 3 => ḅ⁻¹ :: l
+      | _ => []
+      end
   end.
+
+Definition path_of_nat n := path_of_nat_aux (S n) n.
+
+Compute (path_of_nat 0).
+Compute (path_of_nat 1).
+Compute (path_of_nat 2).
+Compute (path_of_nat 3).
+Compute (path_of_nat 4).
+Compute (path_of_nat 5).
+Compute (path_of_nat 6).
+Compute (path_of_nat 7).
+Compute (path_of_nat 8).
+Compute (path_of_nat 9).
+Compute (path_of_nat 10).
+Compute (path_of_nat 11).
+Compute (path_of_nat 12).
+Compute (path_of_nat 13).
+Compute (path_of_nat 14).
+Compute (path_of_nat 15).
+Compute (path_of_nat 16).
+Compute (path_of_nat 17).
+Compute (path_of_nat 18).
+Compute (path_of_nat 19).
+Compute (path_of_nat 20).
 
 bbb.
-
-Fixpoint list_bool_of_pos p :=
-  match p with
-  | xH => []
-  | xO p => false :: list_bool_of_pos p
-  | xI p => true :: list_bool_of_pos p
-  end.
-
-Compute (list_bool_of_pos 5).
-
-Fixpoint path_of_list_bool lb :=
-  match lb with
-  | false :: false :: lb' => ạ :: path_of_list_bool lb'
-  | true :: false :: lb' => ạ⁻¹ :: path_of_list_bool lb'
-  | false :: true :: lb' => ḅ :: path_of_list_bool lb'
-  | true :: true :: lb' => ḅ⁻¹ :: path_of_list_bool lb'
-  | _ => []
-  end.
-
-Compute (path_of_list_bool (list_bool_of_pos 8)).
-
-Print path_of_pos.
-
-Compute (path_of_pos 16).
-
-bbb.
-
-Fixpoint path_of_N n :=
-  match N.modulo n 4 with
-  | N0 => ạ ::
-      match N with
-      | N0 => []
-      | Npos p =>
-
-path_of_N (N.div n 4)
-  | Npos p => []
-  end.
-
-Fixpoint path_of_nat n :=
-  match n mod 4 with
-  | 0 => ạ :: path_of_nat (n / 4)
-  | 1 => ạ⁻¹ :: path_of_nat (n / 4)
-  | 2 => ḅ :: path_of_nat (n / 4)
-  | 3 => ḅ⁻¹ :: path_of_nat (n / 4)
-  | S n' => []
-  end.
-
-Check path_of_nat.
 
 Theorem toto : ∃ (f : nat → list free_elem),
   (∀ n, norm_list (f n) = f n) ∧
