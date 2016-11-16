2e9a493ba407864e4930782ecefa93b8d9e413d4
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-11-16 23:16:04 +0100

    -

--- Banach_Tarski.v
+++ Banach_Tarski.v
@@ -321,6 +321,81 @@ destruct m.
  exists (S (nat_of_free_elem e)).
  now destruct e as (t, d); destruct t, d.
 
+(**)
+ subst el.
+ remember (m / 4) as md eqn:Hmd; symmetry in Hmd.
+ revert e m Hmd.
+ induction md; intros.
+  remember (m mod 4) as mm eqn:Hmm; symmetry in Hmm.
+  destruct mm.
+   remember (1 * 4)%nat as p eqn:Hp.
+   exists (S (nat_of_free_elem e) + p)%nat.
+   simpl; rewrite Nat.sub_0_r.
+   destruct (lt_dec (S (nat_of_free_elem e + p)) 5) as [He| He].
+    exfalso; apply Nat.nle_gt in He; apply He.
+    rewrite Nat.add_comm; subst p; simpl.
+    do 5 apply -> Nat.succ_le_mono.
+    apply Nat.le_0_l.
+
+    subst p; rewrite Nat.mod_add; [ simpl | easy ].
+    rewrite Nat.sub_0_r, Hmd, Hmm.
+    rewrite nat_div_add_once; [ | easy ].
+    destruct (lt_dec (S m) 5) as [Hm| Hm].
+     f_equal; [ now destruct e as (t, d); destruct t, d | ].
+     now destruct e as (t, d); destruct t, d.
+
+     apply Nat.nlt_ge in Hm.
+     apply Nat.succ_le_mono in Hm.
+     apply Nat.div_le_mono with (c := 4) in Hm; [ | easy ].
+     rewrite Hmd in Hm.
+     now apply Nat.nle_succ_0 in Hm.
+
+   destruct mm.
+    remember (2 * 4)%nat as p eqn:Hp.
+    exists (S (nat_of_free_elem e) + p)%nat.
+    simpl; rewrite Nat.sub_0_r.
+    destruct (lt_dec (S (nat_of_free_elem e + p)) 5) as [He| He].
+     exfalso; apply Nat.nle_gt in He; apply He.
+     rewrite Nat.add_comm; subst p; simpl.
+     do 5 apply -> Nat.succ_le_mono.
+     apply Nat.le_0_l.
+
+     subst p; rewrite Nat.mod_add; [ | easy ].
+bbb.
+
+     now destruct e as (t, d); destruct t, d.
+bbb.
+    exists (S (nat_of_free_elem e) + p)%nat.
+    destruct (lt_dec (S (nat_of_free_elem e + p)) 5) as [He| He].
+     exfalso; apply Nat.nle_gt in He; apply He.
+     rewrite Nat.add_comm; subst p; simpl.
+     do 5 apply -> Nat.succ_le_mono.
+     apply Nat.le_0_l.
+
+     subst p; rewrite Nat.mod_add; [ simpl | easy ].
+     rewrite Nat.sub_0_r, Hmd, Hmm.
+bbb.
+     rewrite nat_div_add_once; [ | easy ].
+     destruct (lt_dec (S m) 5) as [Hm| Hm].
+      f_equal; [ now destruct e as (t, d); destruct t, d | ].
+      now destruct e as (t, d); destruct t, d.
+
+      apply Nat.nlt_ge in Hm.
+      apply Nat.succ_le_mono in Hm.
+      apply Nat.div_le_mono with (c := 4) in Hm; [ | easy ].
+      rewrite Hmd in Hm.
+      now apply Nat.nle_succ_0 in Hm.
+
+bbb.
+
+   destruct e as (t, d); destruct t, d; simpl.
+   now destruct e as (t, d); destruct t, d.
+bbb.
+
+  idtac.
+
+
+bbb.
  simpl in IHel; rewrite Nat.sub_0_r in IHel; subst el.
  destruct (lt_dec (S m) 5) as [Hm| Hm].
   remember (m mod 4) as mm eqn:Hmm; symmetry in Hmm.
