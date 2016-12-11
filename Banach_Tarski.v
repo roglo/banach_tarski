aea5fd0bacfa5ea52a197f427488f137ce9344d7
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-12-11 19:43:42 +0100

    -

--- Banach_Tarski.v
+++ Banach_Tarski.v
@@ -739,6 +739,9 @@ unfold fixpoint_of_nat.
 do 2 rewrite path_of_nat_inv.
 apply rotate_rev_path in Hs.
 rewrite <- Hs; f_equal.
+unfold fixpoint_of_path.
+SearchAbout rotation_fixpoint.
+About matrix_fixpoint_ok.
 (* actually, there are two possible fixpoints p₁ and -p₁ *)
 bbb.
 
