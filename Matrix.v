6cab26424b06a347d0d58de043a39bde461bc878
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-12-22 06:57:11 +0100

    -

--- Matrix.v
+++ Matrix.v
@@ -6,11 +6,507 @@
 (* Coq v8.6 *)
 
 Require Import Utf8 List.
+Import ListNotations.
 Require Import Reals Psatz.
 
 Require Import Words Normalize Reverse MiscReals.
 
+Record matrix A (m n : nat) := mkmat
+  { def : A; mat : list (list A) }.
+
+Arguments def [A] [m] [n] _.
+Arguments mat [A] [m] [n] _.
+
+Definition matel {A m n} (M : matrix A m n) i j :=
+  List.nth (pred j) (List.nth (pred i) (mat M) []) (def M).
+
+Definition mat_map {A B m n} (f : A → B) (M : matrix A m n) :=
+  mkmat _ m n (f (def M)) (map (map f) (mat M)).
+
 Inductive point := P : ℝ → ℝ → ℝ → point.
+
+Definition mat_vec_mul (M : matrix _ 3 3) '(P x y z) :=
+  P (matel M 1 1 * x + matel M 1 2 * y + matel M 1 3 * z)
+    (matel M 2 1 * x + matel M 2 2 * y + matel M 2 3 * z)
+    (matel M 3 1 * x + matel M 3 2 * y + matel M 3 3 * z).
+
+Definition mkrmat a₁₁ a₁₂ a₁₃ a₂₁ a₂₂ a₂₃ a₃₁ a₃₂ a₃₃ :=
+  mkmat ℝ 3 3 0%R [[a₁₁; a₁₂; a₁₃]; [a₂₁; a₂₂; a₂₃]; [a₃₁; a₃₂; a₃₃]].
+
+Definition rot_x := mkrmat
+  1         0         0
+  0         (1/3)     (-2*√2/3)
+  0         (2*√2/3)  (1/3).
+Definition rot_inv_x := mkrmat
+  1         0         0
+  0         (1/3)     (2*√2/3)
+  0         (-2*√2/3) (1/3).
+Definition rot_z := mkrmat
+  (1/3)     (-2*√2/3) 0
+  (2*√2/3)  (1/3)     0
+  0         0         1.
+Definition rot_inv_z := mkrmat
+  (1/3)     (2*√2/3)  0
+  (-2*√2/3) (1/3)     0
+  0         0         1.
+
+Definition mat_of_elem e :=
+  match e with
+  | ạ => rot_x
+  | ạ⁻¹ => rot_inv_x
+  | ḅ => rot_z
+  | ḅ⁻¹ => rot_inv_z
+  end.
+
+Definition rotate e pt := mat_vec_mul (mat_of_elem e) pt.
+
+Definition a₁₁ (M : matrix ℝ 3 3) := matel M 1 1.
+Definition a₁₂ (M : matrix ℝ 3 3) := matel M 1 2.
+Definition a₁₃ (M : matrix ℝ 3 3) := matel M 1 3.
+Definition a₂₁ (M : matrix ℝ 3 3) := matel M 2 1.
+Definition a₂₂ (M : matrix ℝ 3 3) := matel M 2 2.
+Definition a₂₃ (M : matrix ℝ 3 3) := matel M 2 3.
+Definition a₃₁ (M : matrix ℝ 3 3) := matel M 3 1.
+Definition a₃₂ (M : matrix ℝ 3 3) := matel M 3 2.
+Definition a₃₃ (M : matrix ℝ 3 3) := matel M 3 3.
+
+Definition mat_mul m₁ m₂ :=
+  mkrmat
+    (a₁₁ m₁ * a₁₁ m₂ + a₁₂ m₁ * a₂₁ m₂ + a₁₃ m₁ * a₃₁ m₂)
+    (a₁₁ m₁ * a₁₂ m₂ + a₁₂ m₁ * a₂₂ m₂ + a₁₃ m₁ * a₃₂ m₂)
+    (a₁₁ m₁ * a₁₃ m₂ + a₁₂ m₁ * a₂₃ m₂ + a₁₃ m₁ * a₃₃ m₂)
+    (a₂₁ m₁ * a₁₁ m₂ + a₂₂ m₁ * a₂₁ m₂ + a₂₃ m₁ * a₃₁ m₂)
+    (a₂₁ m₁ * a₁₂ m₂ + a₂₂ m₁ * a₂₂ m₂ + a₂₃ m₁ * a₃₂ m₂)
+    (a₂₁ m₁ * a₁₃ m₂ + a₂₂ m₁ * a₂₃ m₂ + a₂₃ m₁ * a₃₃ m₂)
+    (a₃₁ m₁ * a₁₁ m₂ + a₃₂ m₁ * a₂₁ m₂ + a₃₃ m₁ * a₃₁ m₂)
+    (a₃₁ m₁ * a₁₂ m₂ + a₃₂ m₁ * a₂₂ m₂ + a₃₃ m₁ * a₃₂ m₂)
+    (a₃₁ m₁ * a₁₃ m₂ + a₃₂ m₁ * a₂₃ m₂ + a₃₃ m₁ * a₃₃ m₂).
+
+Definition mat_id :=
+  mkrmat
+    1 0 0
+    0 1 0
+    0 0 1.
+
+Delimit Scope mat_scope with mat.
+Notation "m₁ * m₂" := (mat_mul m₁ m₂) : mat_scope.
+
+Theorem eq_point_dec : ∀ p₁ p₂ : point, { p₁ = p₂ } + { p₁ ≠ p₂ }.
+Proof.
+intros (x₁, y₁, z₁) (x₂, y₂, z₂).
+destruct (Req_dec x₁ x₂) as [H₁| H₁]; [ subst x₂ | right ].
+ destruct (Req_dec y₁ y₂) as [H₁| H₁]; [ subst y₂ | right ].
+  destruct (Req_dec z₁ z₂) as [H₁| H₁]; [ now subst z₂; left | right ].
+  now intros H; injection H; intros.
+
+ now intros H; injection H; intros.
+
+now intros H; injection H; intros.
+Qed.
+
+Theorem rmat_eq_dec {m n} : ∀ m₁ m₂ : matrix ℝ m n, { m₁ = m₂ } + { m₁ ≠ m₂ }.
+Proof.
+intros.
+pose proof list_eq_dec Req_dec as Rlist_eq_dec.
+destruct (Req_dec (def m₁) (def m₂)) as [Hd| Hd].
+ destruct (list_eq_dec Rlist_eq_dec (mat m₁) (mat m₂)) as [Hrll| Hrll].
+  now left; destruct m₁, m₂; simpl in Hd, Hrll; subst.
+
+  now right; intros H; apply Hrll; subst m₂.
+
+ now right; intros H; apply Hd; subst m₂.
+Qed.
+
+Theorem mat_mul_id_l : ∀ m, mat_mul mat_id m = m.
+Proof.
+intros m.
+unfold mat_mul, mat_id; simpl.
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rplus_0_l.
+progress repeat rewrite Rplus_0_r.
+destruct m.
+unfold mkrmat; simpl.
+f_equal.
+bbb.
+
+now destruct m.
+Qed.
+
+Theorem mat_mul_id_r : ∀ m, mat_mul m mat_id = m.
+Proof.
+intros m.
+unfold mat_mul, mat_id; simpl.
+progress repeat rewrite Rmult_1_r.
+progress repeat rewrite Rmult_0_r.
+progress repeat rewrite Rplus_0_l.
+progress repeat rewrite Rplus_0_r.
+now destruct m.
+Qed.
+
+Theorem mat_vec_mul_id : ∀ p, mat_vec_mul mat_id p = p.
+Proof.
+intros (x, y, z).
+unfold mat_vec_mul; simpl.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rplus_0_l.
+progress repeat rewrite Rplus_0_r.
+easy.
+Qed.
+
+Theorem mat_vec_mul_assoc : ∀ m₁ m₂ p,
+  mat_vec_mul (m₁ * m₂)%mat p = mat_vec_mul m₁ (mat_vec_mul m₂ p).
+Proof.
+intros m₁ m₂ (x, y, z).
+unfold mat_vec_mul.
+simpl; f_equal; lra.
+Qed.
+
+Theorem rot_rot_inv_x : ∀ pt,
+  mat_vec_mul rot_x (mat_vec_mul rot_inv_x pt) = pt.
+Proof.
+intros.
+unfold mat_vec_mul; simpl.
+destruct pt as (x, y, z).
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite Rplus_0_l.
+f_equal.
+ field_simplify; simpl.
+ unfold Rdiv.
+ progress repeat rewrite Rmult_1_r.
+ progress repeat rewrite RMicromega.Rinv_1.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+
+ unfold Rdiv.
+ field_simplify; simpl.
+ progress repeat rewrite Rmult_1_r.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+Qed.
+
+Theorem rot_inv_rot_x : ∀ pt,
+  mat_vec_mul rot_inv_x (mat_vec_mul rot_x pt) = pt.
+Proof.
+intros.
+unfold mat_vec_mul; simpl.
+destruct pt as (x, y, z).
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite Rplus_0_l.
+f_equal.
+ field_simplify; simpl.
+ unfold Rdiv.
+ progress repeat rewrite Rmult_1_r.
+ progress repeat rewrite RMicromega.Rinv_1.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+
+ unfold Rdiv.
+ field_simplify; simpl.
+ progress repeat rewrite Rmult_1_r.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+Qed.
+
+Theorem rot_rot_inv_z : ∀ pt,
+  mat_vec_mul rot_z (mat_vec_mul rot_inv_z pt) = pt.
+Proof.
+intros.
+unfold mat_vec_mul; simpl.
+destruct pt as (x, y, z).
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite Rplus_0_l.
+f_equal.
+ field_simplify; simpl.
+ unfold Rdiv.
+ progress repeat rewrite Rmult_1_r.
+ progress repeat rewrite RMicromega.Rinv_1.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+
+ unfold Rdiv.
+ field_simplify; simpl.
+ progress repeat rewrite Rmult_1_r.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+Qed.
+
+Theorem rot_inv_rot_z : ∀ pt,
+  mat_vec_mul rot_inv_z (mat_vec_mul rot_z pt) = pt.
+Proof.
+intros.
+unfold mat_vec_mul; simpl.
+destruct pt as (x, y, z).
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite Rplus_0_l.
+f_equal.
+ field_simplify; simpl.
+ unfold Rdiv.
+ progress repeat rewrite Rmult_1_r.
+ progress repeat rewrite RMicromega.Rinv_1.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+
+ unfold Rdiv.
+ field_simplify; simpl.
+ progress repeat rewrite Rmult_1_r.
+ rewrite sqrt_sqrt; [ | lra ].
+ field_simplify; simpl.
+ unfold Rdiv.
+ now field_simplify.
+Qed.
+
+Theorem rotate_rotate_neg : ∀ e p, rotate e (rotate (negf e) p) = p.
+Proof.
+intros (t, d) p; simpl.
+destruct t, d; simpl.
+ apply rot_inv_rot_x.
+ apply rot_rot_inv_x.
+ apply rot_inv_rot_z.
+ apply rot_rot_inv_z.
+Qed.
+
+Theorem rotate_neg_rotate : ∀ e p, rotate (negf e) (rotate e p) = p.
+Proof.
+intros (t, d) p; simpl.
+destruct t, d; simpl.
+ apply rot_rot_inv_x.
+ apply rot_inv_rot_x.
+ apply rot_rot_inv_z.
+ apply rot_inv_rot_z.
+Qed.
+
+Theorem app_path_rev_path : ∀ p el,
+  fold_right rotate p (rev_path el ++ el) = p.
+Proof.
+intros.
+revert p.
+induction el as [| e] using rev_ind; intros; [ easy | simpl ].
+rewrite rev_path_app; simpl.
+rewrite app_assoc, fold_right_app; simpl.
+rewrite IHel; apply rotate_neg_rotate.
+Qed.
+
+Theorem rotate_cancel_in : ∀ el₁ el₂ e p,
+  fold_right rotate p (el₁ ++ e :: negf e :: el₂) =
+  fold_right rotate p (el₁ ++ el₂).
+Proof.
+intros.
+do 2 rewrite fold_right_app; simpl.
+now rewrite rotate_rotate_neg.
+Qed.
+
+Theorem rotate_rotate_norm : ∀ el p,
+  fold_right rotate p el = fold_right rotate p (norm_list el).
+Proof.
+intros el p.
+remember (length el) as len eqn:Hlen; symmetry in Hlen.
+revert el p Hlen.
+induction len as (len, IHlen) using lt_wf_rec; intros.
+destruct (norm_list_dec el) as [H₁| H₁]; [ now rewrite H₁ | ].
+destruct H₁ as (el₁ & t & d & el₂ & H₁).
+subst el.
+rewrite rotate_cancel_in, norm_list_cancel_in.
+destruct len; [ now destruct el₁ | ].
+destruct len.
+ destruct el₁; [ easy | simpl in Hlen ].
+ now destruct el₁.
+
+ apply IHlen with len.
+  transitivity (S len); apply Nat.lt_succ_diag_r.
+
+  clear - Hlen.
+  revert len el₂ Hlen.
+  induction el₁ as [| e₁]; intros.
+   simpl in Hlen; simpl.
+   now do 2 apply Nat.succ_inj in Hlen.
+
+   simpl in Hlen; simpl.
+   apply Nat.succ_inj in Hlen.
+   destruct len.
+    destruct el₁; [ easy | simpl in Hlen ].
+    now destruct el₁.
+
+    f_equal.
+    now apply IHel₁.
+Qed.
+
+Theorem rotate_rev_path : ∀ el p₁ p₂,
+  fold_right rotate p₁ el = p₂
+  → fold_right rotate p₂ (rev_path el) = p₁.
+Proof.
+intros el p₁ p₂ Hr.
+revert p₁ p₂ Hr.
+induction el as [| e]; intros; [ now symmetry | ].
+simpl in Hr.
+rewrite rev_path_cons, rev_path_single, fold_right_app; simpl.
+apply IHel; rewrite <- Hr.
+rewrite rotate_neg_rotate.
+easy.
+Qed.
+
+Definition mat_transp m :=
+  mkrmat
+   (a₁₁ m) (a₂₁ m) (a₃₁ m)
+   (a₁₂ m) (a₂₂ m) (a₃₂ m)
+   (a₁₃ m) (a₂₃ m) (a₃₃ m).
+
+Definition mat_det m :=
+  (a₁₁ m * (a₂₂ m * a₃₃ m - a₃₂ m * a₂₃ m) +
+   a₁₂ m * (a₂₃ m * a₃₁ m - a₃₃ m * a₂₁ m) +
+   a₁₃ m * (a₂₁ m * a₃₂ m - a₃₁ m * a₂₂ m))%R.
+
+Arguments mat_transp m%mat.
+Arguments mat_det m%mat.
+
+Theorem mat_transp_id : mat_transp mat_id = mat_id.
+Proof. easy. Qed.
+
+Theorem mat_transp_mul : ∀ m₁ m₂,
+  mat_transp (mat_mul m₁ m₂) = mat_mul (mat_transp m₂) (mat_transp m₁).
+Proof.
+intros m₁ m₂.
+unfold mat_transp, mat_mul; simpl; f_equal; ring.
+Qed.
+
+Theorem mat_mul_assoc : ∀ m₁ m₂ m₃,
+  (m₁ * (m₂ * m₃) = m₁ * m₂ * m₃)%mat.
+Proof.
+intros m₁ m₂ m₃.
+unfold mat_mul; simpl; f_equal; ring.
+Qed.
+
+Theorem mat_det_mul : ∀ m₁ m₂,
+  mat_det (m₁ * m₂) = (mat_det m₂ * mat_det m₁)%R.
+Proof.
+intros m₁ m₂.
+unfold mat_det; simpl; ring.
+Qed.
+
+Definition is_rotation_matrix A :=
+  mat_mul A (mat_transp A) = mat_id ∧
+  mat_det A = 1%R.
+
+Arguments is_rotation_matrix A%mat.
+
+Theorem mat_id_is_rotation_matrix : is_rotation_matrix mat_id.
+Proof.
+split; [ now rewrite mat_transp_id, mat_mul_id_l | ].
+unfold mat_det; simpl; ring.
+Qed.
+
+Theorem rot_x_is_rotation_matrix : is_rotation_matrix rot_x.
+Proof.
+unfold is_rotation_matrix, mat_transp, mat_mul, mat_det; simpl.
+unfold mat_id, Rdiv.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rmult_0_r.
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rplus_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite <- Rmult_assoc.
+progress repeat (rewrite Rmult5_sqrt2_sqrt5; [ | lra ]).
+split; [ f_equal; field | field ].
+Qed.
+
+Theorem rot_inv_x_is_rotation_matrix : is_rotation_matrix rot_inv_x.
+Proof.
+unfold is_rotation_matrix, rot_inv_x, mat_transp, mat_mul, mat_det; simpl.
+unfold mat_id, Rdiv.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rmult_0_r.
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rplus_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite <- Rmult_assoc.
+progress repeat (rewrite Rmult5_sqrt2_sqrt5; [ | lra ]).
+split; [ f_equal; field | field ].
+Qed.
+
+Theorem rot_z_is_rotation_matrix : is_rotation_matrix rot_z.
+Proof.
+unfold is_rotation_matrix, mat_transp, mat_mul, mat_det; simpl.
+unfold mat_id, Rdiv.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rmult_0_r.
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rplus_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite Rminus_0_l.
+progress repeat rewrite Rminus_0_r.
+progress repeat rewrite Ropp_mult_distr_l.
+progress repeat rewrite <- Rmult_assoc.
+progress repeat (rewrite Rmult5_sqrt2_sqrt5; [ | lra ]).
+split; [ f_equal; field | field ].
+Qed.
+
+Theorem rot_inv_z_is_rotation_matrix : is_rotation_matrix rot_inv_z.
+Proof.
+unfold is_rotation_matrix, rot_inv_x, mat_transp, mat_mul, mat_det; simpl.
+unfold mat_id, Rdiv.
+progress repeat rewrite Rmult_0_l.
+progress repeat rewrite Rmult_0_r.
+progress repeat rewrite Rmult_1_l.
+progress repeat rewrite Rplus_0_l.
+progress repeat rewrite Rplus_0_r.
+progress repeat rewrite Rminus_0_l.
+progress repeat rewrite Rminus_0_r.
+progress repeat rewrite Ropp_mult_distr_l.
+progress repeat rewrite <- Rmult_assoc.
+progress repeat (rewrite Rmult5_sqrt2_sqrt5; [ | lra ]).
+split; [ f_equal; field | field ].
+Qed.
+
+Theorem rotate_is_rotation_matrix : ∀ e, is_rotation_matrix (mat_of_elem e).
+Proof.
+intros (t, d); destruct t, d.
+ apply rot_inv_x_is_rotation_matrix.
+ apply rot_x_is_rotation_matrix.
+ apply rot_inv_z_is_rotation_matrix.
+ apply rot_z_is_rotation_matrix.
+Qed.
+
+Theorem mat_mul_is_rotation_matrix : ∀ m1 m2,
+  is_rotation_matrix m1
+  → is_rotation_matrix m2
+  → is_rotation_matrix (m1 * m2).
+Proof.
+intros * (Hm1, Hd1) (Hm2, Hd2).
+unfold is_rotation_matrix.
+rewrite mat_transp_mul.
+rewrite mat_mul_assoc.
+setoid_rewrite <- mat_mul_assoc at 2.
+rewrite Hm2, mat_mul_id_r, Hm1.
+split; [ easy | ].
+rewrite mat_det_mul, Hd1, Hd2.
+apply Rmult_1_r.
+Qed.
+
+bbb.
+
 Record matrix A := mkmat
   { a₁₁ : A; a₁₂ : A; a₁₃ : A;
     a₂₁ : A; a₂₂ : A; a₂₃ : A;
@@ -41,6 +537,8 @@ Definition mt i j :=
 Arguments mt i%nat j%nat [A] m.
 
 Definition mkrmat := @mkmat ℝ.
+
+Inductive point := P : ℝ → ℝ → ℝ → point.
 
 Definition mat_vec_mul mat '(P x y z) :=
   P (a₁₁ mat * x + a₁₂ mat * y + a₁₃ mat * z)
