40ea8cfd0f778a9954cc5f058747959f5dcdc84c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-01-04 10:47:52 +0100

    -

--- Banach_Tarski.v
+++ Banach_Tarski.v
@@ -297,12 +297,14 @@ Definition unit_interv := mkset (λ x, (0 <= x < 1)%R).
 
 Definition ter_bin_of_point '(P x y z) := ter_bin_of_frac_part x.
 
-Theorem ter_bin_of_sphere_surj : ∀ (u : ℕ → bool),
-  ∃ p : point, p ∈ sphere ∧ (∀ n, ter_bin_of_point p n = u n).
+Theorem ter_bin_of_sphere_surj : ∀ r (u : ℕ → bool),
+  ∃ p : point, p ∈ sphere_ray r ∧ (∀ n, ter_bin_of_point p n = u n).
 Proof.
 intros.
-specialize (ter_bin_of_frac_part_surj u); intros (r & Hr & Hn).
+specialize (ter_bin_of_frac_part_surj u); intros (s & Hr & Hn).
 exists (P r 0 0); simpl.
+bbb.
+
 split; [ | easy ].
 do 2 rewrite Rsqr_pow2.
 rewrite pow_i; [ | apply Nat.lt_0_succ ].
