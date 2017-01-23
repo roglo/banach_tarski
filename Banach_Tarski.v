3bd32a392affca2f6df5479838c0b4fdff1a6d88
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-01-23 14:21:51 +0100

    -

--- Banach_Tarski.v
+++ Banach_Tarski.v
@@ -2836,6 +2836,7 @@ assert (Hs : (√ (1 + x²) ≠ 0)%R).
  assert (Hca : ∀ x, cos (atan x) ≠ 0%R).
   intros y Hy.
   specialize (cos_eq_0_0 _ Hy) as (k, Hay).
+  specialize (atan_bound y) as Hab.
 
 bbb.
  apply Rmult_eq_reg_r with (r := √ (1 + x²)); [ | easy ].
