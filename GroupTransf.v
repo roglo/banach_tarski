3995f21e7853d59ae2b40123809df14a793daf1b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-03-21 11:25:06 +0100

    -

--- GroupTransf.v
+++ GroupTransf.v
@@ -19,9 +19,10 @@ Inductive Gr :=
 Definition set_map {A B} (f : A → B) s := mkset (λ v, f v ∈ s).
 
 (* this is strange: I can make a set by set_map, but it does not apply the
-   function of the elements of the set, it creates a set whose application
-   of the function is an element of the set; for the rotation, it means that
-   set_map applies the *inverse* rotation of all elements of the set! *)
+   function of the elements of the set: rather, it creates a set whose
+   application of the function is an element of the set; for the rotation,
+   it means that set_map applies the *inverse* rotation of all elements of
+   the set! *)
 Fixpoint app_gr f p :=
   match f with
   | Rot M Hrm => set_map (mat_vec_mul M) p
