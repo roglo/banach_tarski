c0ca4565febf6ed60ec62f7b016219ac733aa58b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2024-09-21 06:15:51 +0200

    reindent Normalize.v

--- Normalize.v
+++ Normalize.v
@@ -21,48 +21,48 @@ intros e el el₁ el₂.
 revert el₁.
 induction el as [| e₁]; intros; [ intros H; now destruct el₁ | ].
 simpl; remember (norm_list el) as nl eqn:Hnl; symmetry in Hnl.
-destruct nl as [| e₂].
- clear; intros H.
- destruct el₁ as [| e₂]; intros; [ easy | simpl in H ].
- injection H; clear H; intros; subst e₂.
- now destruct el₁.
-
- destruct (letter_opp_dec e₁ e₂) as [H₁| H₁].
+destruct nl as [| e₂]. {
+  clear; intros H.
+  destruct el₁ as [| e₂]; intros; [ easy | simpl in H ].
+  injection H; clear H; intros; subst e₂.
+  now destruct el₁.
+}
+destruct (letter_opp_dec e₁ e₂) as [H₁| H₁]. {
   intros H; subst nl.
   pose proof IHel (e₂ :: el₁) as H₂; simpl in H₂.
   now apply H₂.
-
-  unfold letter_opp in H₁.
-  destruct e₁ as (x₁, d₁).
-  destruct e₂ as (x₂, d₂).
-  destruct (letter_dec x₁ x₂) as [H₂| H₂].
-   subst x₂.
-   destruct (Bool.bool_dec d₁ d₂) as [H₂| H₂].
+}
+unfold letter_opp in H₁.
+destruct e₁ as (x₁, d₁).
+destruct e₂ as (x₂, d₂).
+destruct (letter_dec x₁ x₂) as [H₂| H₂]. {
+  subst x₂.
+  destruct (Bool.bool_dec d₁ d₂) as [H₂| H₂]. {
     clear H₁; subst d₂.
-    destruct el₁ as [| e₁].
-     simpl; intros H.
-     injection H; clear H; intros H₁ H₂ H₃; subst e.
-     simpl in H₂.
-     injection H₂; clear H₂; intros H.
-     symmetry in H; revert H; apply Bool.no_fixpoint_negb.
-
-     simpl; intros H.
-     injection H; clear H; intros H₁ H₂; subst e₁.
-     eapply IHel, H₁.
-
-    exfalso; apply H₁; constructor.
-
-   clear H₁.
-   destruct el₁ as [| e₁].
+    destruct el₁ as [| e₁]. {
+      simpl; intros H.
+      injection H; clear H; intros H₁ H₂ H₃; subst e.
+      simpl in H₂.
+      injection H₂; clear H₂; intros H.
+      symmetry in H; revert H; apply Bool.no_fixpoint_negb.
+    }
     simpl; intros H.
-    injection H; clear H; intros H₁ H₃ H₄; subst e.
-    simpl in H₃.
-    injection H₃; clear H₃; intros; subst x₂ d₂.
-    now apply H₂.
-
-    simpl; intros H.
-    injection H; clear H; intros H₁ H₃.
+    injection H; clear H; intros H₁ H₂; subst e₁.
     eapply IHel, H₁.
+  }
+  exfalso; apply H₁; constructor.
+}
+clear H₁.
+destruct el₁ as [| e₁]. {
+  simpl; intros H.
+  injection H; clear H; intros H₁ H₃ H₄; subst e.
+  simpl in H₃.
+  injection H₃; clear H₃; intros; subst x₂ d₂.
+  now apply H₂.
+}
+simpl; intros H.
+injection H; clear H; intros H₁ H₃.
+eapply IHel, H₁.
 Qed.
 
 Theorem norm_list_no_consec2 : ∀ e el el₁ el₂,
@@ -92,32 +92,32 @@ Theorem norm_list_cancel : ∀ el e,
 Proof.
 intros el (t, d).
 revert t d.
-induction el as [| (t₁, d₁)]; intros.
- simpl; now rewrite letter_dec_diag, bool_dec_negb_r.
-
- remember (FE t₁ d₁ :: el) as el₁ eqn:Hel₁.
- symmetry in Hel₁; simpl.
- remember (norm_list el₁) as el₂ eqn:Hel₂.
- symmetry in Hel₂; simpl.
- destruct el₂ as [| (t₂, d₂)].
+induction el as [| (t₁, d₁)]; intros. {
+  simpl; now rewrite letter_dec_diag, bool_dec_negb_r.
+}
+remember (FE t₁ d₁ :: el) as el₁ eqn:Hel₁.
+symmetry in Hel₁; simpl.
+remember (norm_list el₁) as el₂ eqn:Hel₂.
+symmetry in Hel₂; simpl.
+destruct el₂ as [| (t₂, d₂)]. {
   now rewrite letter_dec_diag, bool_dec_negb_r.
-
-  subst el₁.
-  destruct (letter_dec t t₂) as [H₁| H₁].
-   subst t₂.
-   destruct (Bool.bool_dec (negb d) d₂) as [H₁| H₁].
+}
+subst el₁.
+destruct (letter_dec t t₂) as [H₁| H₁]. {
+  subst t₂.
+  destruct (Bool.bool_dec (negb d) d₂) as [H₁| H₁]. {
     subst d₂.
     now rewrite letter_dec_diag, bool_dec_negb_r.
-
-    apply negb_neq in H₁; subst d₂.
-    destruct el₂ as [| (t₂, d₂)]; [ easy | ].
-    destruct (letter_dec t t₂) as [H₁| H₁]; [ | easy ].
-    subst t₂.
-    destruct (Bool.bool_dec d d₂) as [H₁| H₁]; [ easy | ].
-    apply not_eq_sym, neq_negb in H₁; subst d₂.
-    exfalso; revert Hel₂; apply norm_list_no_start.
-
-   now rewrite letter_dec_diag, bool_dec_negb_r.
+  }
+  apply negb_neq in H₁; subst d₂.
+  destruct el₂ as [| (t₂, d₂)]; [ easy | ].
+  destruct (letter_dec t t₂) as [H₁| H₁]; [ | easy ].
+  subst t₂.
+  destruct (Bool.bool_dec d d₂) as [H₁| H₁]; [ easy | ].
+  apply not_eq_sym, neq_negb in H₁; subst d₂.
+  exfalso; revert Hel₂; apply norm_list_no_start.
+}
+now rewrite letter_dec_diag, bool_dec_negb_r.
 Qed.
 
 Theorem norm_list_cancel_in : ∀ el₁ el₂ e,
@@ -126,11 +126,11 @@ Theorem norm_list_cancel_in : ∀ el₁ el₂ e,
 Proof.
 intros.
 revert el₂ e.
-induction el₁ as [| e₁]; intros.
- do 2 rewrite app_nil_l.
- apply norm_list_cancel.
-
- simpl; now rewrite IHel₁.
+induction el₁ as [| e₁]; intros. {
+  do 2 rewrite app_nil_l.
+  apply norm_list_cancel.
+}
+simpl; now rewrite IHel₁.
 Qed.
 
 Theorem norm_list_cancel_in2 : ∀ el₁ el₂ e,
@@ -150,12 +150,12 @@ intros.
 revert el₁ el₃.
 induction el₂ as [| e₂]; intros; [ easy | simpl ].
 remember (norm_list el₂) as el eqn:Hel₂; symmetry in Hel₂.
-destruct el as [| e].
- simpl in IHel₂; simpl.
- rewrite cons_comm_app, app_assoc.
- now rewrite IHel₂, <- app_assoc.
-
- destruct (letter_opp_dec e₂ e) as [H₁| H₁].
+destruct el as [| e]. {
+  simpl in IHel₂; simpl.
+  rewrite cons_comm_app, app_assoc.
+  now rewrite IHel₂, <- app_assoc.
+}
+destruct (letter_opp_dec e₂ e) as [H₁| H₁]. {
   apply letter_opp_negf in H₁; subst e₂.
   rewrite cons_comm_app.
   do 2 rewrite app_assoc.
@@ -163,12 +163,12 @@ destruct el as [| e].
   do 2 rewrite <- app_assoc; simpl.
   rewrite norm_list_cancel_in2.
   easy.
-
-  rewrite cons_comm_app.
-  do 2 rewrite app_assoc.
-  rewrite <- IHel₂.
-  do 2 rewrite <- app_assoc.
-  easy.
+}
+rewrite cons_comm_app.
+do 2 rewrite app_assoc.
+rewrite <- IHel₂.
+do 2 rewrite <- app_assoc.
+easy.
 Qed.
 
 Theorem norm_list_normal_l : ∀ el₁ el₂,
@@ -207,15 +207,15 @@ induction el as [| e₁]; intros; [ easy | ].
 remember (e₁ :: el) as el₁ eqn:Hel.
 simpl in Hn.
 remember (norm_list el₁) as el₂ eqn:Hel₁; symmetry in Hel₁.
-destruct el₂ as [| e₂].
- injection Hn; clear Hn; intros; now subst.
-
- destruct (letter_opp_dec e e₂) as [H₁| H₁].
+destruct el₂ as [| e₂]. {
+  injection Hn; clear Hn; intros; now subst.
+}
+destruct (letter_opp_dec e e₂) as [H₁| H₁]. {
   apply letter_opp_negf in H₁; subst e el₂.
   exfalso; revert Hel₁; apply norm_list_no_start.
-
-  injection Hn; clear Hn; intros; subst el₁.
-  easy.
+}
+injection Hn; clear Hn; intros; subst el₁.
+easy.
 Qed.
 
 Theorem norm_list_app_diag : ∀ el₁ el₂,
@@ -240,20 +240,20 @@ Theorem norm_list_dec : ∀ el,
 Proof.
 intros el.
 induction el as [| e]; [ now left | ].
-destruct IHel as [IHel| IHel].
- simpl.
- rewrite IHel.
- destruct el as [| e₁]; [ now left | ].
- destruct (letter_opp_dec e e₁) as [H₁| H₁]; [ right | now left ].
- apply letter_opp_sym, letter_opp_negf in H₁; subst e₁.
- destruct e as (t, d).
- exists [], t, d, el.
- easy.
-
- right.
- destruct IHel as (el₁, (t, (d, (el₂, IHel)))).
- exists (e :: el₁), t, d, el₂; subst el.
- easy.
+destruct IHel as [IHel| IHel]. {
+  simpl.
+  rewrite IHel.
+  destruct el as [| e₁]; [ now left | ].
+  destruct (letter_opp_dec e e₁) as [H₁| H₁]; [ right | now left ].
+  apply letter_opp_sym, letter_opp_negf in H₁; subst e₁.
+  destruct e as (t, d).
+  exists [], t, d, el.
+  easy.
+}
+right.
+destruct IHel as (el₁, (t, (d, (el₂, IHel)))).
+exists (e :: el₁), t, d, el₂; subst el.
+easy.
 Qed.
 
 Theorem norm_list_repeat : ∀ e n, norm_list (repeat e n) = repeat e n.
@@ -291,30 +291,30 @@ Theorem norm_list_app_split : ∀ el₁ el₂ el₃ el₄ e,
 Proof.
 intros el₁ el₂ el₃ el₄ e Hn.
 apply split_app_eq in Hn.
-destruct Hn as [(el, (H₁, H₂))| (el, (H₁, H₂))].
- exfalso; revert H₂; apply norm_list_no_consec.
-
- rewrite app_of_cons in H₂.
- apply split_app_eq in H₂.
- destruct H₂ as [(el', (H₂, H₃))| (el', (H₂, H₃))].
+destruct Hn as [(el, (H₁, H₂))| (el, (H₁, H₂))]. {
+  exfalso; revert H₂; apply norm_list_no_consec.
+}
+rewrite app_of_cons in H₂.
+apply split_app_eq in H₂.
+destruct H₂ as [(el', (H₂, H₃))| (el', (H₂, H₃))]. {
   subst el.
-  destruct el' as [| e'].
-   rewrite app_nil_r in H₁.
-   rewrite app_nil_l in H₃; symmetry in H₃.
-   now split.
-
-   simpl in H₃.
-   injection H₃; clear H₃; intros H₂ H₃; subst e'.
-   exfalso; revert H₁; apply norm_list_no_consec.
-
-  destruct el as [| e₁].
-   simpl in H₂; subst el'.
-   exfalso; revert H₃; apply norm_list_no_start.
-
-   simpl in H₂.
-   injection H₂; clear H₂; intros H₂ H₄; subst e₁.
-   symmetry in H₂.
-   apply app_eq_nil in H₂.
-   destruct H₂; subst el el'.
-   now split.
+  destruct el' as [| e']. {
+    rewrite app_nil_r in H₁.
+    rewrite app_nil_l in H₃; symmetry in H₃.
+    now split.
+  }
+  simpl in H₃.
+  injection H₃; clear H₃; intros H₂ H₃; subst e'.
+  exfalso; revert H₁; apply norm_list_no_consec.
+}
+destruct el as [| e₁]. {
+  simpl in H₂; subst el'.
+  exfalso; revert H₃; apply norm_list_no_start.
+}
+simpl in H₂.
+injection H₂; clear H₂; intros H₂ H₄; subst e₁.
+symmetry in H₂.
+apply app_eq_nil in H₂.
+destruct H₂; subst el el'.
+now split.
 Qed.  
