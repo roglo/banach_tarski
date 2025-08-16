94637834b42d159f5235505397105c395537c728
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-08-16 17:30:55 +0200

    -

--- MiscReals.v
+++ MiscReals.v
@@ -1237,12 +1237,56 @@ induction m as [m| m| ]; intros. {
     remember (Pos.to_nat m) as p eqn:H; clear m H; rename p into m.
     remember (Pos.to_nat n) as p eqn:H; clear n H; rename p into n.
     move n before m.
-(* ah bin, on voit bien que Hm et Hn sont incompatibles, mais pour
-   le prouver, hein... encore un truc à la con *)
-...
-Search (_ <= _ + _)%Z.
-      apply pos_le_add.
-Search (_ ~0 <= _ ~1)%positive.
+    rewrite <- rngl_of_nat_2 in Hm, Hn.
+    rewrite <- (rngl_of_nat_mul Hon Hos) in Hm, Hn.
+    rewrite <- rngl_of_nat_1 in Hm.
+    rewrite <- rngl_of_nat_1 in Hn.
+    do 2 rewrite <- rngl_of_nat_add in Hm.
+    rewrite <- rngl_of_nat_add in Hn.
+    (* lemma to do *)
+    rename m into p; rename n into m; rename p into n.
+    rename Hm into H; rename Hn into Hm; rename H into Hn.
+    move m after n; move Hm after Hn.
+    specialize (between_rngl_of_nat_and_succ x x) as H1.
+    specialize (H1 _ _ (rngl_le_refl Hor _) Hm Hn).
+    specialize (between_rngl_of_nat_and_succ x x) as H2.
+    specialize (H2 _ _ (rngl_le_refl Hor _) Hn Hm).
+    apply Nat.le_antisymm in H2; [ | easy ].
+    symmetry in H2.
+    apply Nat.add_sub_eq_l in H2.
+    rewrite <- Nat.mul_sub_distr_l in H2.
+    apply (f_equal Nat.odd) in H2.
+    now rewrite Nat.odd_even in H2.
+  } {
+    exfalso; clear IHm.
+    specialize (Pos2Nat.is_pos m) as Hmz.
+    apply Nat.neq_0_lt_0 in Hmz.
+    rewrite rngl_of_pos_add in Hm, Hn.
+    rewrite rngl_of_pos_1 in Hm, Hn.
+    rewrite rngl_of_pos_xI in Hm.
+    progress unfold rngl_of_pos in Hm.
+    remember (Pos.to_nat m) as p eqn:H; clear m H; rename p into m.
+    rewrite <- rngl_of_nat_2 in Hm, Hn.
+    rewrite <- (rngl_of_nat_mul Hon Hos) in Hm(*, Hn*).
+    rewrite <- rngl_of_nat_1 in Hm.
+    rewrite <- rngl_of_nat_1 in Hn.
+    do 2 rewrite <- rngl_of_nat_add in Hm.
+    (* lemma to do *)
+    rename m into p; (*rename n into m;*) rename p into n.
+    rename Hm into H; rename Hn into Hm; rename H into Hn.
+    (*move m after n; *)move Hm after Hn.
+    specialize (between_rngl_of_nat_and_succ x x) as H1.
+    specialize (H1 _ _ (rngl_le_refl Hor _) Hm Hn).
+    specialize (between_rngl_of_nat_and_succ x x) as H2.
+    specialize (H2 _ _ (rngl_le_refl Hor _) Hn Hm).
+    apply Nat.le_antisymm in H2; [ | easy ].
+    symmetry in H2.
+    apply Nat.add_sub_eq_r in H2.
+    rewrite Nat.sub_diag in H2.
+    symmetry in H2.
+    now apply Nat.eq_mul_0_l in H2.
+  }
+}
 ...
 
 Theorem Int_part_prop :
