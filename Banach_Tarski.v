0a23a460769141bb618863cb8dd277514e000749
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-12-06 12:57:32 +0100

    -

--- Banach_Tarski.v
+++ Banach_Tarski.v
@@ -679,23 +679,40 @@ destruct (rngl_eqb_dec u₁ 0) as [Hu₁| Hu₁]. {
   destruct (rngl_eqb_dec u₂ 0) as [Hu₂| Hu₂]. {
     apply (rngl_eqb_eq Heo) in Hu₂; subst u₂.
     rewrite (rngl_mul_0_l Hos) in H₁; symmetry in H₁.
-...
-    apply Rmult_integral in H₁.
+    apply (rngl_integral Hos Hio) in H₁.
     destruct H₁ as [H₁| H₁]; [ now exfalso; subst u₃; apply Hu | subst v₂ ].
-    exists v₃, (- u₃).
+    exists v₃, (- u₃)%L.
     split; [ now intros H; apply Hv; f_equal | ].
-    split; [ now apply Ropp_neq_0_compat; intros H; apply Hu; f_equal | ].
-    f_equal; lra.
+    split. {
+      intros H.
+      apply (f_equal rngl_opp) in H.
+      rewrite (rngl_opp_involutive Hop), (rngl_opp_0 Hop) in H.
+      now subst u₃.
+    }
+    do 2 rewrite (rngl_mul_0_r Hos).
+    rewrite (rngl_mul_opp_l Hop), (rngl_add_opp_r Hop).
+    rewrite (rngl_mul_comm Hic), (rngl_sub_diag Hos).
+    now rewrite rngl_add_0_l.
   }
-  destruct (Req_dec u₃ 0) as [Hu₃| Hu₃]. {
-    subst u₃; rewrite Rmult_0_l in H₁.
-    apply Rmult_integral in H₁.
+  apply (rngl_eqb_neq Heo) in Hu₂.
+  destruct (rngl_eqb_dec u₃ 0) as [Hu₃| Hu₃]. {
+    apply (rngl_eqb_eq Heo) in Hu₃; subst u₃.
+    rewrite (rngl_mul_0_l Hos) in H₁.
+    apply (rngl_integral Hos Hio) in H₁.
     destruct H₁ as [H₁| H₁]; [ easy | subst v₃ ].
-    exists v₂, (- u₂).
+    exists v₂, (- u₂)%L.
     split; [ now intros H; apply Hv; f_equal | ].
-    split; [ now apply Ropp_neq_0_compat; intros H; apply Hu; f_equal | ].
-    f_equal; lra.
+    split. {
+      intros H.
+      apply (f_equal rngl_opp) in H.
+      now rewrite (rngl_opp_involutive Hop), (rngl_opp_0 Hop) in H.
+    }
+    do 2 rewrite (rngl_mul_0_r Hos).
+    rewrite (rngl_mul_opp_l Hop), (rngl_add_opp_r Hop).
+    rewrite (rngl_mul_comm Hic), (rngl_sub_diag Hos).
+    now rewrite rngl_add_0_l.
   }
+...
   destruct (Req_dec v₂ 0) as [Hv₂| Hv₂]. {
     subst v₂; rewrite Rmult_0_r in H₁.
     apply Rmult_integral in H₁.
